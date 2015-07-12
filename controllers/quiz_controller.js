ddbc9bedf25cd676ef6c4da371555958fe18529d
Santiago Domínguez <Santiago.Dominguez.Barrios@gmail.com>
2015-07-12 17:27:47 +0200

    Funcionalidad de añadir pregunta y de validar

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -14,20 +14,19 @@ exports.load = function(req, res, next, quizId) {
 
 // GET /quizes
 exports.index = function(req, res) {
-  console.log(req.query.search);
   if (!req.query.search) {
     req.query.search='';
   }
   models.Quiz.findAll({where:["pregunta like '%"+ req.query.search.replace(/ /g, "%")  +"%'"]}).then(function(quizes) {
   //models.Quiz.findAll().then(function(quizes) {
-    res.render('quizes/index.ejs', {quizes: quizes});
+    res.render('quizes/index.ejs', {quizes: quizes, errors: []});
   }
   ).catch(function(error) { next(error);})
 };
 
 // GET /quizes/:id
 exports.show = function(req, res) {
-  res.render('quizes/show', {quiz: req.quiz});
+  res.render('quizes/show', {quiz: req.quiz, errors: []});
 }
 
 // GET /quizes/answer
@@ -36,10 +35,39 @@ exports.answer = function(req, res) {
   if (req.query.respuesta === req.quiz.respuesta) {
     resultado = 'Correcto';
   }
-  res.render('quizes/answer', {quiz: req.quiz, respuesta: resultado});
+  res.render('quizes/answer', {quiz: req.quiz, respuesta: resultado, errors: []});
 }
+
+// GET /quizes/new
+exports.new = function(req, res) {
+  var quiz = models.Quiz.build( // crea objeto quiz
+    {pregunta: "Pregunta", respuesta: "Respuesta"}
+  );
+  res.render('quizes/new', {quiz: quiz, errors: []});
+};
+
+// POST /quizes/create
+exports.create = function(req, res) {
+  var quiz = models.Quiz.build( req.body.quiz );
+
+  console.log("quiz.validate():")
+  console.log(quiz.validate());
+  quiz
+  .validate()
+  .then(
+    function(err){
+      if (err) {
+        res.render('quizes/new', {quiz: quiz, errors: err.errors});
+      } else {
+        quiz // save: guarda en DB campos pregunta y respuesta de quiz
+        .save({fields: ["pregunta", "respuesta"]})
+        .then(function(){ res.redirect('/quizes');})
+      }  // Redirección HTTP (URL relativo) lista de preguntas
+    }
+  );
+};
 
 // GET /author
 exports.author = function(req, res) {
-  res.render('author', {miNombre: 'Santiago Domínguez', miFoto: 'images/mifoto.jpeg'});
+  res.render('author', {miNombre: 'Santiago Domínguez', miFoto: 'images/mifoto.jpeg', errors: []});
 };
--- routes/index.js
+++ routes/index.js
@@ -5,7 +5,7 @@ var quizController = require('../controllers/quiz_controller');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Quiz' });
+  res.render('index', { title: 'Quiz', errors: [] });
 });
 
 
@@ -15,6 +15,8 @@ router.param('quizId', quizController.load);  // autoload :quizId
 router.get('/quizes', quizController.index);
 router.get('/quizes/:quizId(\\d+)', quizController.show);
 router.get('/quizes/:quizId(\\d+)/answer', quizController.answer);
+router.get('/quizes/new', quizController.new);
+router.post('/quizes/create', quizController.create);
 router.get('/author', quizController.author);
 
 
