2232a70921afc72a1443d5ccffed54ed3420efdd
Santiago Domínguez <Santiago.Dominguez.Barrios@gmail.com>
2015-07-18 20:04:38 +0200

    Comentarios

--- routes/index.js
+++ routes/index.js
@@ -13,6 +13,7 @@ router.get('/', function(req, res, next) {
 
 // Autoload de comandos con :quizId
 router.param('quizId', quizController.load);  // autoload :quizId
+router.param('commentId', commentController.load);  // autoload :commentId
 
 // Definición de rutas de sesión
 router.get('/login', sessionController.new);      // formulari login
@@ -29,8 +30,12 @@ router.put('/quizes/:quizId(\\d+)', sessionController.loginRequired, quizControl
 router.delete('/quizes/:quizId(\\d+)',  sessionController.loginRequired, quizController.destroy);
 router.get('/author', quizController.author);
 
+// Definición de rutas de comentarios
 router.get('/quizes/:quizId(\\d+)/comments/new', commentController.new);
 router.post('/quizes/:quizId(\\d+)/comments',    commentController.create);
+router.get('/quizes/:quizId(\\d+)/comments/:commentId(\\d+)/publish',
+  sessionController.loginRequired, commentController.publish);
+
 
 
 module.exports = router;
